fae3cf6622b7f8e1c29ef605f8e319facb1fe98d
Mele Naigulevu <102633659+naigulevu2362@users.noreply.github.com>
2022-06-15 23:50:34 +0000

    added nothing

---
+++ 02_variables.py
@@ -0,0 +1,13 @@
+# cade to lernd about variables
+
+# variables can be a string
+greeting = "hello world"
+
+#they can also be numbers
+fav_num = 7
+print(fav_num * 2)
+
+# Below is a constant (the name is in UPPERCASE)
+
+num_string = ("5")
+print(num_string * 5)
---
+++ 03_getting_input.py
@@ -0,0 +1,21 @@
+# Ask the user for theie name
+username = input("What is your name? ")
+
+# Ask the user for their favourite integer
+fav_num = int(input("Favourite Number? "))
+
+# Double, the and square the number
+double = fav_num * 2 
+half = fav_num / 2
+squared = fav_num * fav_num
+
+print()
+# Greet the user
+print("Hi {}, you favourite number is {}".format(username, fav_num))
+print()
+#output thr results of boubling, havling 
+# and squaring their favorite number
+print("Double {} = {}".format(fav_num, double))
+print("double{} id {}".format(fav_num, double))
+print("{} squares is {}". format(fav_num,half))
+print()
